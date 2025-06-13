742c2cd4a4010b558dc6e2e26c4ca6cbc79f920e
jjawang <chadae111@naver.com>
2025-06-13 15:42:22 +0900

    custom 공지사항

--- backend/controllers/buyer/b_dashboardController.js
+++ backend/controllers/buyer/b_dashboardController.js
@@ -4,16 +4,17 @@ const prisma = new PrismaClient();
 const getNotice = async (req, res) => {
   try {
     const newNotice = await prisma.notice.findMany({
+      where: { type: "GENERAL" },
       select:{
         title: true,
         content: true,
         type: true,
         admin_id: true,
-        pinned:true
-      }     
+        pinned:true,
+      }
     });
     console.log(newNotice)
-    res.status(201).json({ message: '공지가 성공적으로 등록되었습니다.', notice: newNotice });
+    res.status(201).json({ notice: newNotice });
   } catch (error) {
     console.error('공지 등록 오류:', error);
     res.status(500).json({ message: '공지 등록 중 오류가 발생했습니다.' });
