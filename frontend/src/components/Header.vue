bc3260e0bfe67ead7e744fa532af5f2e95e15eea
jjawang <chadae111@naver.com>
2025-06-16 18:16:38 +0900

    homepage

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,23 +1,25 @@
 <template>
-  <header class="header">
-    <router-link to="/" class="logo">RemoteR</router-link>
-    <nav class="nav">
-      <router-link to="/">Home</router-link>
-      <a href="#">판매자 등록</a>
-
-      <template v-if="isLoggedIn">
-        <span>{{ userName }}님 환영합니다</span>
-        <router-link to="/seller-dashboard">마이페이지</router-link>
-        <a href="#" @click.prevent="handleLogout">로그아웃</a>
-      </template>
-
-      <template v-else>
-        <a href="#" @click.prevent="handleLoginClick">로그인</a>
-        <router-link to="/signup">
-          <button class="signup-btn">회원가입</button>
-        </router-link>
-      </template>
-    </nav>
+  <header class="header" :class="$attrs.class">
+    <div class="header-box">
+      <router-link to="/" class="logo">RemoteR</router-link>
+      <nav class="nav">
+        <router-link to="/">Home</router-link>
+        <a href="#">판매자 등록</a>
+  
+        <template v-if="isLoggedIn">
+          <span>{{ userName }}님 환영합니다</span>
+          <router-link to="/seller-dashboard">마이페이지</router-link>
+          <a href="#" @click.prevent="handleLogout">로그아웃</a>
+        </template>
+  
+        <template v-else>
+          <a href="#" @click.prevent="handleLoginClick">로그인</a>
+          <router-link to="/signup">
+            <button class="signup-btn">회원가입</button>
+          </router-link>
+        </template>
+      </nav>
+    </div>
   </header>
 </template>
 
@@ -56,23 +58,35 @@ const handleLogout = async () => {
 
 <style>
 .header {
+  margin-bottom: 40px;
+  background: transparent;
+  color: black;
+  display: flex;
+  justify-content: center;
+}
+.header .header-box{
   display: flex;
   justify-content: space-between;
   align-items: center;
-  margin-bottom: 40px;
-  background: #00003e;
-  color: white;
-  padding: 0px 20px;
   height: 60px;
+  width: 1200px;
+  max-width: 1200px;
 }
+.header.scrolled {
+  background-color: rgba(255, 255, 255, 0.493);
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.05);
+  backdrop-filter: blur(10px)
+}
+
 .header span{
   font-size: 14px;
 }
 
 
 .logo {
-  font-size: 20px;
+  font-size: 26px;
   font-weight: bold;
+  color: #000;
 }
 
 .nav {
@@ -83,7 +97,7 @@ const handleLogout = async () => {
 
 .nav a {
   text-decoration: none;
-  color: white;
+  color: black;
   font-size: 14px;
 }
 
