142c3d311065662f15eba22b5cc61d75b7c511ef
jjawang <chadae111@naver.com>
2025-06-27 14:05:47 +0900

    login

--- backend/controllers/seller/authController.js
+++ backend/controllers/seller/authController.js
@@ -34,7 +34,7 @@ const login = async (req, res) => {
     res.cookie('refreshToken', refreshToken, {
       httpOnly : true,
       secure : false,
-      sameSite : 'None',
+      sameSite : 'Lax',
       path : '/',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
