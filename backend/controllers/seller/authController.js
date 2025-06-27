99d167306b7087dd2e77eab90e1191941135e37c
jjawang <chadae111@naver.com>
2025-06-27 13:51:23 +0900

    login

--- backend/controllers/seller/authController.js
+++ backend/controllers/seller/authController.js
@@ -39,7 +39,7 @@ const login = async (req, res) => {
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     
-    console.log(accessToken);
+    console.log("로컬", accessToken);
     
     // accessToken만 클라이언트에 전달
     res.json({ token : accessToken, user: { user_id: user.user_id, name: user.name, email: user.email } });
